62b2a205d97703d965b7a5669ca9ffefe2410035
0teklee <leetekwoo@gmail.com>
2025-03-11 22:21:40 +0900

    fix(eslint-config): ignores dir (algorithms/content/*)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,43 +9,43 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: eslint.configs.recommended
+  baseDirectory: __dirname,
+  recommendedConfig: eslint.configs.recommended,
 });
 
 export default [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    prettier,
-    {
-        ignores: ["algorithms/**/*"],
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    ignores: ["algorithms/content/*"],
+  },
+  {
+    files: ["uiux/**/*.{js,jsx,ts,tsx,mdx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
     },
-    {
-        files: ["uiux/**/*.{js,jsx,ts,tsx,mdx}"],
-        languageOptions: {
-            parser: tseslint.parser,
-            parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module"
-            }
-        },
-        plugins: {
-            "@typescript-eslint": tseslint.plugin,
-        },
-        rules: {
-            ...tseslint.configs.recommended.rules,
-            "prettier/prettier": "error"
-        }
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
     },
-    {
-        files: ["algorithms/**/*.{js,jsx}"],
-        languageOptions: {
-            ecmaVersion: 2022,
-            sourceType: "module",
-            env: {
-                node: true,
-                es2022: true
-            }
-        }
-    }
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    files: ["algorithms/**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      env: {
+        node: true,
+        es2022: true,
+      },
+    },
+  },
 ];
