84374adf18ee6b2f39bd7ddc6c390bb87d0f2ecf
0teklee <leetekwoo@gmail.com>
2025-03-12 15:47:09 +0900

    feature(tailwind-config): add: darkMode = "class", theme: screens, colors, borderRadius. namespace matched to global.css variables

--- site/config/tailwind.config.ts
+++ site/config/tailwind.config.ts
@@ -1,12 +1,46 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "../../algorithms/site/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "../../uiux/site/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        tablet: "600px",
+        widescreen: "1536px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        theme: "hsl(var(--theme))",
+        background: "hsl(var(--background))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          bg: "hsl(var(--secondary-bg))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
