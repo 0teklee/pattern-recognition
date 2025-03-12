8919ba76ae7d78cd0f983e6f00290744e468b086
0teklee <leetekwoo@gmail.com>
2025-03-12 13:14:31 +0900

    refactor(astro-config): fixed directory + config settings : Check comment in base.config.mjs

---
+++ site/config/algorithms.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import { baseConfig } from "./base.config.mjs";
+
+/**
+ * @type {Object}
+ * @define property를 제외한 나머지는 base config 유지
+ * @property {string} outDir - "../dist/algorithms"
+ * @description build:algo 시 @/site/algorithms/templates의 변경 사항만 빌드. outDir 제외 나머지 baseConfig 유지
+ * @see base.config.mjs - 기본 설정 사항 확인
+ * **/
+const algoConfig = {
+  ...baseConfig,
+  outDir: "../dist/algorithms",
+  build: {
+    format: "directory",
+  },
+};
+
+export default defineConfig(algoConfig);
---
+++ site/config/uiux.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import { baseConfig } from "./base.config.mjs";
+
+/**
+ * @type {Object}
+ * @define property를 제외한 나머지는 base config 유지
+ * @property {string} outDir - "../dist/uiux"
+ * @property {Array} integrations - 프레임워크 통합 플러그인 배열
+ * @description build:uiux 시 @/site/uiux/templates의 변경 사항만 빌드.
+ * @see base.config.mjs - 기본 설정
+ * **/
+const uiuxConfig = {
+  ...baseConfig,
+  integrations: [...(baseConfig.integrations || []), react()],
+  outDir: "../dist/uiux",
+  build: {
+    format: "directory",
+  },
+};
+
+export default defineConfig(uiuxConfig);
