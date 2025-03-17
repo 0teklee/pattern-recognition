bef3278f989aa681e322a2847e2f8aa82c10c6da
0teklee <leetekwoo@gmail.com>
2025-03-17 12:22:49 +0900

    refactor(types): 1. renamed Domains -> Routes, 2. removed enum -> changed to "as const" var 3. applied custom util type KeysOf, SubsetOfKeys

--- site/types/domains.ts
+++
@@ -1,9 +0,0 @@
-enum Domains {
-  Index = "index",
-  Algorithms = "algorithms",
-  Uiux = "uiux",
-}
-
-type SubRoot = Exclude<Domains, Domains.Index>;
-
-export type { Domains, SubRoot };
---
+++ site/types/routes.ts
@@ -0,0 +1,14 @@
+import type { KeysOf, SubsetOfKeys } from "@site/types/utility.ts";
+
+const Routes = {
+  index: "/",
+  algorithms: "/algorithms",
+  uiux: "/uiux",
+} as const;
+
+type RouteType = typeof Routes;
+type RouteKeys = KeysOf<RouteType>;
+
+type SubRoutes = SubsetOfKeys<RouteType, "algorithms" | "uiux">;
+
+export type { Routes, SubRoutes, RouteKeys, RouteType };
