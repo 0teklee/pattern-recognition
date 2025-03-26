ea3fb018b92bae1c11825abfc7e4d138c1024a24
0teklee <leetekwoo@gmail.com>
2025-03-26 10:42:12 +0900

    refactor: RouteKeyType to schema

--- site/types/schema.ts
+++ site/types/schema.ts
@@ -1,8 +1,8 @@
 import type { SearchParam } from "@site/types/query.ts";
-import type { RouteKeys } from "@site/types/routes.ts";
+import type { RouteKeyType } from "@site/types/routes.ts";
 
 /**@type {BaseSchema}**/
-interface BaseContentSchema<T extends RouteKeys = "index"> {
+interface BaseContentSchema<T extends RouteKeyType = "index"> {
   title: string;
   createdAt: string;
   tags: SearchParam<T>;
