449974cc4bec47ef17791e60ed1d03113796f46f
0teklee <leetekwoo@gmail.com>
2025-03-26 11:01:06 +0900

    refactor: Route constants updates & removed unused types

--- site/types/props.ts
+++ site/types/props.ts
@@ -1,26 +1,7 @@
-import type { SearchParam } from "@site-types/query";
 import type { HTMLAttributes } from "astro/types";
-import type { RouteKeys, RouteType, SubRoutes } from "site/types/routes.ts";
 
 // Inject meta title/description in @site/pages
 export interface MetaTagProps extends Omit<HTMLAttributes<"meta">, "dir"> {
   title?: string;
   description?: string;
-}
-
-/** Inject & Rendering {title | desc | searchParam}
- *  in @site/pages/{algo | uiux}/index.astro **/
-export interface ContentListProps extends MetaTagProps {
-  path: SubRoutes;
-  searchParam?: SearchParam<SubRoutes> | null;
-}
-
-/** Inject & Rendering {title | desc | searchParam}
- *  in @site/pages/{algo | uiux}/{path}/index.astro
- *  **/
-export interface ContentMetaDataProps<T extends RouteKeys> {
-  title: string;
-  createdAt: string;
-  tags: string[];
-  path: [RouteType[T], ...string[]];
 }
